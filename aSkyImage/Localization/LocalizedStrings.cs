09b585d797e577559d7c621afbed16bc6bc92cd6
Antti Karjakin <antti.karjakin@gmail.com>
2013-10-27 14:20:40 +0200

    localization basics

---
+++ aSkyImage/Localization/LocalizedStrings.cs
@@ -0,0 +1,19 @@
+﻿using aSkyImage.Resources;
+
+namespace aSkyImage.Localization
+{
+    public class LocalizedStrings
+    {
+        public LocalizedStrings()
+        {
+
+        }
+
+        private static AppResources localizedResources = new AppResources();
+
+        public AppResources AppResources
+        {
+            get { return localizedResources; }
+        }
+    }
+}
